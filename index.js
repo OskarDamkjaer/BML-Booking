fdea413bcd85a2e58664325856e3eec616497a07
Anna Palmqvist Sjövall <anna_sjovall96@hotmail.com>
2017-01-16 16:40:39 +0100

    Added mög

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-var http = require("http");
+var server = require("./server");
+var router = require("./router");
 
-http.createServer(pageCalled).listen(8081);
-
-function pageCalled(request, response){
-	response.writeHead(200, {'Content-Type':'text/plain'});
-
-	response.end('Hello World\n');
-}
-
-console.log("Server running at http://127.0.0.1:8081/");
+server.start(router.route);
